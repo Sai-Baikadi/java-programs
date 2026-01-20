989d5577ca725cc9167b1bdf2e8e26efc6b9f727
Sai-Baikadi <bsaicharang@gmail.com>
2026-01-20 21:06:33 +0530

    initial commit

---
+++ src/com/stschool/java/programs/PrintNumbers.java
@@ -0,0 +1,13 @@
+package com.stschool.java.programs;
+
+public class PrintNumbers {
+    public static void main(String[] args){
+        int n = 10;
+        int i=1;
+        while(i<=n){
+
+            System.out.println(i);
+            i++;
+        }
+    }
+}
